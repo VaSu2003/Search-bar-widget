c32761961fc7698c78db1f9bdeec00ebac8b14db
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-07-27 13:23:41 +0530

    Fix - Show only those results which are typed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ async function fetchProducts(){
         const response = await fetch('https://fakestoreapi.com/products')
         const json = await response.json()
         productsObj = json
+        console.log(productsObj)
         
     } catch (error) {
         console.log("there was an error")
@@ -16,29 +17,31 @@ fetchProducts();
 
 
 let input = document.querySelector('#search')
-
+let matchingProducts;
 input.addEventListener('input',(e)=>{
-    setTimeout(()=>{
-        const matchingProducts = productsObj.filter((product)=>{
+        // count++;
+        matchingProducts = productsObj.filter((product)=>{
             if(e.target.value){
                 return product.title.toLowerCase().includes((e.target.value).toLowerCase())
             }
         })
-        console.log(matchingProducts)
+
         const search_res = document.querySelector('#search_res')
+        search_res.innerHTML = ''
+        
+        // console.log(matchingProducts)
         if(matchingProducts.length !== 0){
             matchingProducts.map((ele)=>{
                 const li = document.createElement('li')
                 li.textContent = ele.title
                 search_res.appendChild(li)
             })
-        }
-    },2000)
-    
-    
-   
+        }       
+           
 
 })
+
+console.log(matchingProducts)
 
 
 
