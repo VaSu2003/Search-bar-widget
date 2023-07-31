a703978b8bbae3f1d27ec2a0bf969573f13b5184
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-07-31 13:23:42 +0530

    add debouncing to limit the api calls

--- index.js
+++ index.js
@@ -17,31 +17,39 @@ fetchProducts();
 
 let input = document.querySelector('#search')
 let matchingProducts;
-input.addEventListener('input',(e)=>{
-        // count++;
-        matchingProducts = productsObj.filter((product)=>{
-            if(e.target.value){
-                return product.title.toLowerCase().includes((e.target.value).toLowerCase())
-            }
+
+input.addEventListener('input',debounce())
+
+function debounce (){
+
+    let debounceTimer;
+    return function(e){
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(()=>{
+            console.log('productsObj called')
+            matchingProducts = productsObj.filter((product)=>{
+        if(e.target.value){
+            return product.title.toLowerCase().includes((e.target.value).toLowerCase())
+        }
+    })
+
+    const search_res = document.querySelector('#search_res')
+    search_res.innerHTML = ''
+    
+    // console.log(matchingProducts)
+    if(matchingProducts.length !== 0){
+        matchingProducts.map((ele)=>{
+            const li = document.createElement('li')
+            li.classList.add('listItem')
+            li.textContent = ele.title
+            search_res.appendChild(li)
         })
+    }      
+        },300)
+    }
+     
+       
 
-        const search_res = document.querySelector('#search_res')
-        search_res.innerHTML = ''
-        
-        // console.log(matchingProducts)
-        if(matchingProducts.length !== 0){
-            matchingProducts.map((ele)=>{
-                const li = document.createElement('li')
-                li.classList.add('listItem')
-                li.textContent = ele.title
-                search_res.appendChild(li)
-            })
-        }       
-           
-
-})
-
-
-
+}
 
 
